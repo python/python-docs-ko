5129e49c12ecde830205f55924adbbaa52d1bda0
오동권 <flowdas@gmail.com>
2025-04-14 13:26:39 +0000

    pdk init, build & watch

--- .pdk/pdk/__init__.py
+++ .pdk/pdk/__init__.py
@@ -1,10 +1,66 @@
+import os
+import pathlib
+import shutil
+import subprocess
+
 import fire
+
+MSG_DIR = pathlib.Path(__file__).parent.parent.parent
+
+
+def sh(cmd, capture=False, chdir=None):
+    opts = {
+        "shell": True,
+        "stdin": subprocess.PIPE,
+    }
+    cwd = os.getcwd() if chdir else None
+    if chdir:
+        os.chdir(chdir)
+    try:
+        if capture:
+            opts["stderr"] = subprocess.STDOUT
+            opts["universal_newlines"] = True
+            return subprocess.check_output(cmd, **opts)
+        else:
+            return subprocess.check_call(cmd, **opts)
+    finally:
+        if cwd:
+            os.chdir(cwd)
+
+
+def create_symlink(symlink, to):
+    np = len(pathlib.Path(os.path.commonpath([symlink, to])).parts)
+    parts = ("..",) * (len(symlink.parts) - np - 1) + to.parts[np:]
+    relpath = os.path.sep.join(parts)
+    symlink.parent.mkdir(parents=True, exist_ok=True)
+    symlink.symlink_to(relpath, target_is_directory=to.is_dir())
+
+
+def chdir_Doc():
+    os.chdir(MSG_DIR / "../cpython/Doc")
 
 
 class Command:
     def init(self):
         """Initialize .pdk."""
-        pass
+        os.chdir(MSG_DIR / "..")
+        if pathlib.Path("cpython").exists():
+            shutil.rmtree("cpython")
+        sh(f"git clone --single-branch -b {os.environ['PDK_BRANCH']} https://github.com/python/cpython")
+        sh(f"git checkout {os.environ['PDK_REVISION']}", chdir="cpython")
+        LC_MESSAGES = pathlib.Path(
+            "cpython/Doc/locales/ko/LC_MESSAGES").absolute()
+        create_symlink(LC_MESSAGES, MSG_DIR)
+
+    def build(self):
+        """Build translated document."""
+        chdir_Doc()
+        sh("make -e SPHINXOPTS=\"-D language='ko'\" html")
+
+    def watch(self):
+        """Rebuild translated document on changes, with hot reloading in the browser."""
+        chdir_Doc()
+        sh("make -e SPHINXOPTS=\"-D language='ko'\" htmllive")
 
 
 def main():
